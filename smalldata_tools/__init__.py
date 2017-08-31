d30d55f61cf214e6daa9ffc8cde6edf5c968ef55
Silke Nelson <snelson@slac.stanford.edu>
2017-08-31 00:50:32 -0700

    add detector for wave8 waveforms, fix ttRawDetector

--- smalldata_tools/__init__.py
+++ smalldata_tools/__init__.py
@@ -6,7 +6,7 @@ from DetObject import  ROIObject,DetObject
 from droplet import  aduHist,dropletSave,photonizeDrops,droplet
 from fitCenter import  fitCenter
 from photons import  photon,photon2,photon3
-from SmallDataUtils import  defaultDetectors,lightStatus,ipmDetector,bmmonDetector,epicsDetector,encoderDetector,controlDetector,aiDetector,ttDetector,damageDetector,ttRawDetector,xtcavDetector,detData,getCfgOutput,getUserData,getUserEnvData,defaultRedisVars
+from SmallDataUtils import  defaultDetectors,lightStatus,ipmDetector,bmmonDetector,epicsDetector,encoderDetector,controlDetector,aiDetector,ttDetector,damageDetector,ttRawDetector,xtcavDetector,detData,getCfgOutput,getUserData,getUserEnvData,defaultRedisVars,wave8Detector
 
 #from SmallDataAna_psana import  SmallDataAna_psana
 #from SmallDataAna import  photons,droplets,Cube,Selection,SmallDataAna
