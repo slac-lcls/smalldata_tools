37419c892804a675bf1310e9c6a7ca59c75073b0
Silke Nelson <snelson@slac.stanford.edu>
2018-12-06 22:18:27 -0800

    remove dbug statement

--- smalldata_tools/DetObject.py
+++ smalldata_tools/DetObject.py
@@ -486,7 +486,6 @@ class DetObject(dropObject):
         print 'no data for', self._name,' , let mpiDataSource take care of this'
         return
       for ROI in self.getROIs():
-        print 'proc ROI'
         if self.mask is not None:
           ROI.area=ROI.applyROI(np.ma.masked_array(self.evt.dat, ~(self.mask.astype(bool))))
         else:
