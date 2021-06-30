925e5ff091f856707dcdd17c9c72790bc6794ff4
Silke Nelson <snelson@slac.stanford.edu>
2021-06-30 12:03:33 -0700

    add support for more detector types for saving masks

--- smalldata_tools/SmallDataAna_psana.py
+++ smalldata_tools/SmallDataAna_psana.py
@@ -1444,10 +1444,20 @@ class SmallDataAna_psana(object):
                 dirname='/reg/d/psdm/%s/%s/calib/CsPad::CalibV1/%s/pixel_mask/'%(self.expname[:3],self.expname,srcStr)        
             elif det.dettype==13:
                 dirname='/reg/d/psdm/%s/%s/calib/Epix100a::CalibV1/%s/pixel_mask/'%(self.expname[:3],self.expname,srcStr)
-            elif det.dettype==19:
+            elif det.dettype in [5,6,8,9,19,27,36]:
                 dirname='/reg/d/psdm/%s/%s/calib/Camera::CalibV1/%s/pixel_mask/'%(self.expname[:3],self.expname,srcStr)        
+            elif det.dettype==26:
+                dirname='/reg/d/psdm/%s/%s/calib/Jungfrau::CalibV1/%s/pixel_mask/'%(self.expname[:3],self.expname,srcStr)        
             elif det.dettype==32:
                 dirname='/reg/d/psdm/%s/%s/calib/Epix10ka2M::CalibV1/%s/pixel_mask/'%(self.expname[:3],self.expname,srcStr)        
+            elif det.dettype==33:
+                dirname='/reg/d/psdm/%s/%s/calib/Epix10kaQuad::CalibV1/%s/pixel_mask/'%(self.expname[:3],self.expname,srcStr)        
+            elif det.dettype==29:
+                dirname='/reg/d/psdm/%s/%s/calib/Epix10ka::CalibV1/%s/pixel_mask/'%(self.expname[:3],self.expname,srcStr)        
+            else:
+                print('This detector type is not yet supported, we will return.')
+                return mask
+
             if not os.path.exists(dirname):
                 os.makedirs(dirname)
             fname='%s-end.data'%self.run
