0d995d552a2c96c6a09e93d4870e08acada7857e
Silke Nelson <snelson@slac.stanford.edu>
2022-03-06 19:45:52 -0800

    work properly with tiled detectors, rewrite imageFunc so that you can add functions to the result

--- smalldata_tools/ana_funcs/droplet.py
+++ smalldata_tools/ana_funcs/droplet.py
@@ -42,6 +42,7 @@ class dropletFunc(DetObjectFunc):
         if self._mask is None and det.mask is not None:
             setattr(self, '_mask', det.mask.astype(np.uint8))
         setattr(self, '_rms', det.rms)
+        setattr(self, '_needsGeo', det._needsGeo)
         if not self.useRms:
             self._compData = np.ones_like(self._mask)
         else:
@@ -185,15 +186,18 @@ class dropletFunc(DetObjectFunc):
             pos_drop = np.array(measurements.center_of_mass(img,imgDrop, drop_ind_thres))
             npix_drop = (measurements.sum(img.astype(bool).astype(int),imgDrop, drop_ind_thres)).astype(int)
             dat_dict={'data': drop_adu}#adu_drop}
-            #dat_dict={'data': adu_drop}
+            dat_dict['npix']=npix_drop
             if drop_adu.shape[0]==0:
                 dat_dict['row']=np.array([])
                 dat_dict['col']=np.array([])
+                if self._needsGeo:
+                    dat_dict['tile']=np.array([])
             else:
-                dat_dict['row']=pos_drop[:,0]
-                dat_dict['col']=pos_drop[:,1]
-            dat_dict['npix']=npix_drop
+                dat_dict['row']=pos_drop[:,pos_drop.shape[1]-2]
+                dat_dict['col']=pos_drop[:,pos_drop.shape[1]-1]
+                dat_dict['tile']=pos_drop[:,0]
         else:
+            #this should be tested for tiled detectors!
             #t2 = time.time()
             self.regions = measure.regionprops(imgDrop, intensity_image=img, cache=True)
             dropSlices = measurements.find_objects(imgDrop)
