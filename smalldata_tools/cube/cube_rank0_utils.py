856d98dd2624cb3bb62d5179060e1f33c5906a0e
Vincent Esposito <espov@slac.stanford.edu>
2022-03-11 13:02:19 -0800

    cube report histogram still work if it fails for one of the variable

--- smalldata_tools/cube/cube_rank0_utils.py
+++ smalldata_tools/cube/cube_rank0_utils.py
@@ -40,45 +40,52 @@ def make_report(anaps, cube_infos, hist_list, filters, varList, exp, run):
     # HISTOGRAMS AND FILTERS
     filts = []
     for ii,(varName, hist_param) in enumerate(hist_list.items()):
-        var = anaps.sda.getVar(varName)
-        low = [filt[1] for filt in filters if filt[0]==varName]
-        high = [filt[2] for filt in filters if filt[0]==varName]
+        try:
+            var = anaps.sda.getVar(varName)
+            low = [filt[1] for filt in filters if filt[0]==varName]
+            high = [filt[2] for filt in filters if filt[0]==varName]
         
-        if low and high:
-            filts.append( np.logical_and.reduce([var>low[0], var<high[0]]) )
-        else:
-            filts.append(None)
+            if low and high:
+                filts.append( np.logical_and.reduce([var>low[0], var<high[0]]) )
+            else:
+                filts.append(None)
+        except:
+            continue
             
     for ii,(varName, hist_param) in enumerate(hist_list.items()):
-        var = anaps.sda.getVar(varName)
-        filt_var = np.logical_and.reduce([f for j,f in enumerate(filts) if (f is not None) and (j!=ii)])
-        var_filtered = var[filt_var]
-        low = [filt[1] for filt in filters if filt[0]==varName]
-        high = [filt[2] for filt in filters if filt[0]==varName]
+        try:
+            var = anaps.sda.getVar(varName)
+            filt_var = np.logical_and.reduce([f for j,f in enumerate(filts) if (f is not None) and (j!=ii)])
+            var_filtered = var[filt_var]
+            low = [filt[1] for filt in filters if filt[0]==varName]
+            high = [filt[2] for filt in filters if filt[0]==varName]
         
-        if low and high:
-            ratio = np.logical_or.reduce([var<low[0], var>high[0]]).sum()/var.size
-            ratio_filtered = np.logical_or.reduce([var_filtered<low[0], var_filtered>high[0]]).sum()/var_filtered.size
-            title = '{}. Filter: {} - {}. Rejected: {:.1%} / {:.1%}'.format(varName, low, high, ratio, ratio_filtered)
-        else:
-            title = '{}'.format(varName)
-        if hist_param is None:
-            bins = np.linspace(np.percentile(var,0.01), np.percentile(var,99.9), 60)
-        else:
-            bins = np.linspace(hist_param[0], hist_param[1], hist_param[2])
-        counts, edges = np.histogram(var, bins=bins)
-        counts_filtered, edges = np.histogram(var_filtered, bins=bins)
-        fig = figure(title=title, x_axis_label=varName, y_axis_label='Count', 
-            background_fill_color="#fafafa")
-        fig.quad(top=counts, bottom=0, left=edges[:-1], right=edges[1:], line_color='black', fill_color='#000080')
-        fig.quad(top=counts_filtered, bottom=0, left=edges[:-1], right=edges[1:], line_color='black', 
-                 fill_color='#59CBE8', fill_alpha=0.7)
-        if low and high:
-            l1 = Span(location=low[0], dimension='height', line_color='orange', line_width=2)
-            l2 = Span(location=high[0], dimension='height', line_color='orange', line_width=2)
-            fig.renderers.extend([l1,l2])
-        l = ii%2 # is left
-        hists_tab[((ii+1)//2-l):((ii+1)//2-l+1),l:l+1] = fig
+            if low and high:
+                ratio = np.logical_or.reduce([var<low[0], var>high[0]]).sum()/var.size
+                ratio_filtered = np.logical_or.reduce([var_filtered<low[0], var_filtered>high[0]]).sum()/var_filtered.size
+                title = '{}. Filter: {} - {}. Rejected: {:.1%} / {:.1%}'.format(varName, low, high, ratio, ratio_filtered)
+            else:
+                title = '{}'.format(varName)
+            if hist_param is None:
+                bins = np.linspace(np.percentile(var,0.01), np.percentile(var,99.9), 60)
+            else:
+                bins = np.linspace(hist_param[0], hist_param[1], hist_param[2])
+            counts, edges = np.histogram(var, bins=bins)
+            counts_filtered, edges = np.histogram(var_filtered, bins=bins)
+            fig = figure(title=title, x_axis_label=varName, y_axis_label='Count', 
+                background_fill_color="#fafafa")
+            fig.quad(top=counts, bottom=0, left=edges[:-1], right=edges[1:], line_color='black', fill_color='#000080')
+            fig.quad(top=counts_filtered, bottom=0, left=edges[:-1], right=edges[1:], line_color='black', 
+                     fill_color='#59CBE8', fill_alpha=0.7)
+            if low and high:
+                l1 = Span(location=low[0], dimension='height', line_color='orange', line_width=2)
+                l2 = Span(location=high[0], dimension='height', line_color='orange', line_width=2)
+                fig.renderers.extend([l1,l2])
+            l = ii%2 # is left
+            hists_tab[((ii+1)//2-l):((ii+1)//2-l+1),l:l+1] = fig
+        except:
+            print(f"Could not make histogram for {varName}")
+            continue
     tabs = pn.Tabs(hists_tab)
 
 
