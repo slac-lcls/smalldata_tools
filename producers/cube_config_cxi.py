ea65b942a3eeca38639a4fa3996ee8e50fe7f655
Tyler Pennebaker <tntp@stanford.edu>
2022-05-07 08:44:36 -0700

    MNT: Update cube_config_cxi with lx87 changes

--- producers/cube_config_cxi.py
+++ producers/cube_config_cxi.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 # Custom bins
+# currently used for timing scans w/ timetool correction where you might want binning !- step size.
 def binBoundaries(run):
     if isinstance(run,str):
         run=int(run)
@@ -14,17 +15,16 @@ def binBoundaries(run):
 # format: list of [det (field), low, high, name]
 # 'filter1' is the standard name and will not be added to the h5 file name.
 filters = [
-    #['lightStatus/xray',0.5,1.5,'filter1'],
+    ['lightStatus/xray',0.5,1.5,'filter1'],
     # ['ipm_dg1/sum',1e3,4.5e4,'filter1'],
-    ['ipm_dg2/sum',-1e6,4.5e4,'filter1'],
-    # ['ipm3/sum',200,3e3,'filter1'],
+    ['ipm_dg2/sum',50,125000.,'filter1'],
     # ['tt/FLTPOSFWHM',60,230,'filter1'],
     # ['tt/AMPL',0.005,0.19,'filter1']
     # ['evr/code_41',0.5,1.5,'custom']
 ]
 
 # Laser on/off
-laser = False
+laser = True
 use_tt = False
 
 # ##### DETECTORS ########################################################################
@@ -33,35 +33,27 @@ use_tt = False
 # More area detector can be defined following the same syntax, and adding them to varList
 detDict = {'source':'jungfrau4M', 
            'full':1, 
-           'image':1, 
-           'thresADU':-1e5, 
-           'common_mode':0}
+           'image':0, 
+           'thresADU':1.2, 
+           'common_mode':30,
+           'maxHis':20}
 
-#pix_size = 176e-6
-#func_kwargs = {
-#    'name': 'azav_pyfai', # must be the name of the smalldata_tools ana_func
-#    'ai_kwargs': {'dist':1, 'poni1':960*pix_size, 'poni2':960*pix_size},
-#    'npts': 512,
-#    'int_units' : '2th_deg',
-#    'return2d' : False
-#}
-#det_proc = [func_kwargs]
-#detDict['det_proc'] = det_proc
 
 # make list of all variables to be added to the cube
-#varList = ['ipm_dg1/sum','ipm_dg2/sum', detDict]
+varList = ['ipm_dg2/sum','ipm_dg3/sum', 'ipm_dg2/peaks','ipm_dg3/peaks', 'jungfrau4M/azav_azav', detDict]
 #varList = [detDict]
-varList = ['ipm_dg2/sum', detDict]
+#varList = ['ipm_dg2/sum', detDict]
 
 # ##### LOGBOOK SUMMARIES #################################################################
 # histogram configuration. Usually does not need to be changed
 # field: destination in smd, list: [low,high,n] or None (then default to some percentile)
 hist_list = {
     # 'ipm_dg1/sum': [0, 5e4, 70],
-    'ipm_dg2/sum': [0, 5e3, 70],
-    #'tt/FLTPOS_PS': [-0.5, 0.5, 70],
-    #'tt/AMPL': [0, 0.2, 70],
-    #'tt/FLTPOSFWHM': [0, 300, 70]
+    'ipm_dg2/sum': [-1000, 1.75e5, 175],
+    'ipm_dg3/sum': [-5e4, 0, 125],
+    'tt/FLTPOS_PS': [-0.5, 0.5, 70],
+    'tt/AMPL': [0, 0.2, 70],
+    'tt/FLTPOSFWHM': [0, 300, 70]
 }
 
 # save as tiff file (ingore unless MEC)
