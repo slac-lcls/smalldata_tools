898c0efb3a063b27d15857502e30f03a9da38ef5
Hannah Zhang <hzhang12@slac.stanford.edu>
2023-05-25 10:33:11 -0700

    relative path, logging module, pytest syntax

--- tests/test_zyla.py
+++ tests/test_zyla.py
@@ -13,9 +13,7 @@ exp = 'xpptut15'
 run = 630
 ds_str = f"exp={exp}:run={run}"
 ds = psana.MPIDataSource(ds_str)
-sys.path.append('/sdf/home/h/hzhang12/smalldata_tools/')
-import smalldata_tools 
-from smalldata_tools.DetObject import DetObjects
+
 det = DetObject('zyla_0', ds.env(), run)
 
 @pytest
