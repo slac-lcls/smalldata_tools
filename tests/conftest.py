bf3c8a9b8fb136eba9d2c460e3fc2ad9b13b7c88
Hannah Zhang <hzhang12@slac.stanford.edu>
2023-06-01 15:51:14 -0700

    detector instantiation tests and test fixtures (already tested)

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,14 +9,14 @@ def datasource(request):
     run = request.param.get('run')
     ds_str = f"exp={exp}:run={run}"
     ds = psana.MPIDataSource(ds_str)
-    return (ds, run)
+    yield (ds, run)
+    
 
 @pytest.fixture(scope="module")
 def detector(request, datasource):
     ds, run = datasource
     detector_name = request.param.get('name')
     detector = DetObject(detector_name, ds.env(), run)
-    return detector
-
+    yield detector
 
    
