c167d2c2754e784920e0ffb3c0446dfd60e0c88d
Hannah Zhang <hzhang12@slac.stanford.edu>
2023-06-15 13:54:56 -0700

    spectrum test and roi_spetrum test

--- tests/test_jungfrau.py
+++ tests/test_jungfrau.py
@@ -255,3 +255,131 @@ def test_ROI_Projection(getROIs, getProjections, detector, datasource):
     os.remove('test_roi_proj.h5')
     
     
+@pytest.mark.parametrize('getSpectrums', [{'bins': [1,2,3,4,5,6,7,8]}], indirect=True)
+@pytest.mark.parametrize('datasource', [{'exp': 'xpptut15', 'run': 650}], indirect=True)
+@pytest.mark.parametrize('detector', [{'name': 'jungfrau1M'}], indirect=True)
+def test_Spectrum(getSpectrums, detector, datasource):
+    logger.info('Running test for Spectrum function to check output format.')
+    try:
+        func_kwargs = getSpectrums
+    except Exception as e:
+        print(f'Can\'t instantiate Projection args: {e}')
+        func_kwargs = []
+    bins = len(func_kwargs['bins'])
+    spec = spectrumFunc(**func_kwargs)
+    det = detector
+    ds, run = datasource
+    det.addFunc(spec)
+    
+    userDict = {}
+    small_data = ds.small_data('./test_spec.h5', gather_interval=5) # file to save data to
+    ds.break_after(5) # stop event loop after 5 events
+
+    for nevt,evt in enumerate(ds.events()): # usual psana event loop
+        det.getData(evt) # get the detector data
+        det.processFuncs() # process the attached functions
+        userDict[det._name]=getUserData(det) # get the function results
+        small_data.event(userDict) # write data to h5
+    
+    # Test the function
+    f = "test_spec.h5"
+    h5explorer = tables.File(f).root
+    
+    #checking for the output
+    assert(list(h5explorer.jungfrau1M._v_children)==['spectrum_histogram'])
+    logger.info('name and number of outputs')
+    
+    #checking for the shape
+    assert(h5explorer.jungfrau1M.spectrum_histogram.shape==(5, bins-1))
+    logger.info('Correct shape for spectrum_histogram')
+    
+    #checking for the type
+    for i in range(5):        
+        assert(h5explorer.jungfrau1M.spectrum_histogram[i].dtype == 'int64')
+    logger.info('Correct data type for spectrum_histogram')
+    
+    logger.info('Pass the Spectrum_function test')
+    
+    tables.file._open_files.close_all()
+    
+    os.remove('test_spec.h5')
+    
+@pytest.mark.parametrize('getROIs', [{'ROI': [[1,2], [157,487], [294,598]], 'writeArea': True, 'thresADU': None, }], indirect=True)
+@pytest.mark.parametrize('getSpectrums', [{'bins': [1,2,3,4,5,6,7,8]}], indirect=True)
+@pytest.mark.parametrize('datasource', [{'exp': 'xpptut15', 'run': 650}], indirect=True)
+@pytest.mark.parametrize('detector', [{'name': 'jungfrau1M'}], indirect=True)
+def test_Spectrum(getROIs, getSpectrums, detector, datasource):
+    logger.info('Running test for ROI_Spectrum function to check output format.')
+    #roi function
+    try:
+        func_kwargs = getROIs
+    except Exception as e:
+        print(f'Can\'t instantiate ROI args: {e}')
+        func_kwargs = []
+    writeArea = func_kwargs['writeArea']
+    [[x1, x2], [y1, y2], [z1, z2]] = func_kwargs['ROI']
+    func = ROIFunc(**func_kwargs)
+    #spectrum function
+    try:
+        func_kwargs = getSpectrums
+    except Exception as e:
+        print(f'Can\'t instantiate Projection args: {e}')
+        func_kwargs = []
+    bins = len(func_kwargs['bins'])
+    spec = spectrumFunc(**func_kwargs)
+    
+    func.addFunc(spec)
+    
+    det = detector
+    ds, run = datasource
+    det.addFunc(func)
+    
+    # Process a couple events
+    userDict = {} # dictionary to store detector data
+    small_data = ds.small_data('./test_roi_spec.h5', gather_interval=5) # file to save data to
+    ds.break_after(5) # stop event loop after 5 events
+
+    for nevt,evt in enumerate(ds.events()): # usual psana event loop
+        det.getData(evt) # get the detector data
+        det.processFuncs() # process the attached functions
+        userDict[det._name]=getUserData(det) # get the function results
+        small_data.event(userDict) # write data to h5
+    
+    # Test the function
+    f = "test_roi_spec.h5"
+    h5explorer = tables.File(f).root
+    
+    #checking for the output
+    if writeArea:
+        assert(list(h5explorer.jungfrau1M._v_children)==['ROI_area', 'ROI_com', 'ROI_max', 'ROI_mean', 'ROI_spectrum_histogram', 'ROI_sum'])
+    else:
+        assert(list(h5explorer.jungfrau1M._v_children)==['ROI_com', 'ROI_max', 'ROI_mean', 'ROI_sum', 'ROI_spectrum_histogram'])
+    logger.info('correct name and number of outputs')
+    
+    #checking for the shape
+    assert(h5explorer.jungfrau1M.ROI_com.shape == (5,2))
+    if writeArea:
+        assert(h5explorer.jungfrau1M.ROI_area.shape == (5,y2-y1,z2-z1))
+    assert(h5explorer.jungfrau1M.ROI_max.shape == (5,))
+    assert(h5explorer.jungfrau1M.ROI_mean.shape == (5,))
+    assert(h5explorer.jungfrau1M.ROI_sum.shape == (5,))
+    assert(h5explorer.jungfrau1M.ROI_spectrum_histogram.shape==(5, bins-1))
+    logger.info('Correct data shape for ROI_area, ROI_com, ROI_max, ROI_mean, ROI_sum and ROI_spectrum_histogram_data')
+    
+    #checking for the type
+    for i in range(5):        
+        assert(h5explorer.jungfrau1M.ROI_spectrum_histogram[i].dtype == 'int64')
+        assert(h5explorer.jungfrau1M.ROI_com[i].dtype == 'float64')
+        assert(h5explorer.jungfrau1M.ROI_max[i].dtype == 'float32')
+        assert(h5explorer.jungfrau1M.ROI_mean[i].dtype == 'float32')
+        assert(h5explorer.jungfrau1M.ROI_sum[i].dtype == 'float32')
+        if writeArea:
+            assert(h5explorer.jungfrau1M.ROI_area[i].dtype == 'float32')
+    logger.info('Correct data type for ROI_area, ROI_com, ROI_max, ROI_mean, ROI_sum and ROI_spectrum_histogram_data')
+     
+    logger.info('Pass the ROI_Spectrum_function test')
+    
+    tables.file._open_files.close_all()
+    
+    os.remove('test_roi_spec.h5')
+    
