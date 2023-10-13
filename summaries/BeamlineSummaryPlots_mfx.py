7b3f8252cca79e0f77c608ac26460ea923f965f2
gadorlhiac <gadorlhiac@gmail.com>
2023-10-13 10:54:34 -0700

    ENH: MFX Beamline summaries, checkpoint after recent experiment

--- summaries/BeamlineSummaryPlots_mfx.py
+++ summaries/BeamlineSummaryPlots_mfx.py
@@ -2,19 +2,23 @@
 ########################################################################
 ## load tools and packages required for data handling and plotting
 ########################################################################
-import panel as pn
-import h5py
+import sys
 import os
 import argparse
 import logging
+import socket
 import requests
-import numpy as np
 from requests.auth import HTTPBasicAuth
+from typing import Optional, Tuple
+
+import h5py
+import numpy as np
+import panel as pn
 import holoviews as hv
 from holoviews import dim
 hv.extension('bokeh')
 pn.extension()
-import sys
+
 try:
     basestring
 except NameError:
@@ -29,6 +33,160 @@ sys.path.append(fpath)
 from smalldata_tools.SmallDataAna_psana import SmallDataAna_psana as sdaps
 from smalldata_tools.utilities import image_from_dxy
 from smalldata_tools.utilities import rebin
+
+def getElogBasicAuth(exp: str) -> HTTPBasicAuth:
+    """Return an authentication object for the eLog API for an opr account.
+
+    This method will only work for active experiments. "opr" accounts are
+    removed from the authorized users list after the experiment ends.
+
+    Paramters
+    ---------
+    exp (str) Experiment name (to determine operator username).
+
+    Returns
+    -------
+    http_auth (HTTPBasicAuth) Authentication for eLog API.
+    """
+    opr_name: str = f"{exp[:3]}opr"
+    hostname: str = socket.gethostname()
+    if hostname.find('sdf') >= 0:
+        auth_path: str = "/sdf/group/lcls/ds/tools/forElogPost.txt"
+    else:
+        auth_path: str = f"/cds/home/opr/{opr_name}/forElogPost.txt"
+
+    with open(auth_path, "r") as f:
+        pw: str = f.readline()[:-1]
+
+    return HTTPBasicAuth(username=opr_name, password=pw)
+
+def postElogMsg(
+        exp: str,
+        msg: str,
+        *,
+        tag: Optional[str] = "",
+        title: Optional[str] = "",
+        files: list = []
+) -> None:
+    """Post a new message to the eLog. Adapted from `elog` package.
+
+    Parameters
+    ----------
+    exp (str) Experiment name.
+    msg (str) Body of the eLog post.
+    tag (str) Optional. A tag to include for the post.
+    title (str) Optional. A title for the eLog post.
+    files (list) Optional. Either a list of paths (str) to files (figures) to
+        include with the eLog post, OR, a list of 2-tuples of strings of the
+        form (`path`, `description`).
+    """
+    post_files: list = []
+    for f in files:
+        if isinstance(f, str):
+            desc: str = os.path.basename(f)
+            formatted_file: tuple = (
+                "files",
+                (desc, open(f, "rb")),
+                mimetypes.guess_type(f)[0]
+            )
+        elif isinstance(f, tuple) or isinstance(f, list):
+            formatted_file: tuple = (
+                "files",
+                (f[1], open(f[0], "rb")),
+                mimetypes.guess_type(f[0])[0]
+            )
+        else:
+            logger.debug(f"Can't parse file {f} for eLog attachment. Skipping.")
+            continue
+        post_files.append(formatted_file)
+
+    post: dict = {}
+    post['log_text'] = msg
+    if tag:
+        post['log_tags'] = tag
+    if title:
+        post['log_title'] = title
+
+    http_auth: HTTPBasicAuth = getElogBasicAuth(exp)
+    base_url: str = "https://pswww.slac.stanford.edu/ws-auth/lgbk/lgbk"
+    post_url: str = f"{base_url}/{exp}/ws/new_elog_entry"
+
+    params: dict = {'url': post_url, 'data': post, 'auth': http_auth}
+    if post_files:
+        params.update({'files': post_files})
+
+    resp: requests.models.Response = requests.post(**params)
+
+    if resp.status_code >= 300:
+        logger.debug(
+            f"Error when posting to eLog: HTTP status code {resp.status_code}"
+        )
+
+    if not resp.json()['success']:
+        logger.debug(f"Error when posting to eLog: {resp.json()['error_msg']}")
+
+def postDetectorDamageMsg(
+        detectors: list,
+        exp: str,
+        run: int,
+        *,
+        tag: str = "SUMMARY_EVENT_DAMAGE",
+        title: str = "EVENT DAMAGE INFO",
+        smd_dir: Optional[str] = None,
+        post_thresh: float = 0.1
+) -> None:
+    """Post detector event damage info for a specified run to the eLog.
+
+    Parameters
+    ----------
+    detectors (list[str]) Names of detectors to report on.
+    exp (str) Experiment name.
+    run (int) Run number. Usually the current run.
+    tag (str) Optional. Tag for the event damage summary posts.
+    title (str) Optional. Title for event damage summary posts.
+    smd_dir (str) Optional. Alternative directory for smalldata HDF5 files.
+    post_thresh (float) Optional. Damage threshold (as a percentage)
+        required to post a message to eLog. At least 1 detector must pass
+        the threshold to post to eLog. Only detectors passing the threshold
+        will be included.
+    """
+    if smd_dir:
+        anaps = sdaps(exp, run, dirname=smd_dir)
+    else:
+        anaps = sdaps(exp, run)
+
+    ana = anaps.sda
+
+    table_header: str = (
+        "<thead><tr><th colspan=\"3\">"
+        f"<center>{title}</center>"
+        "</th></tr></thead>"
+    )
+    table_body: str = (
+        "<tbody><tr>"
+        "<td><b><center>Detector</center></b></td>"
+        "<td><b><center>Missing/Damaged Events</center></b></td>"
+        "<td><b><center>Percentage Missing/Damaged</center></b></td></tr>"
+    )
+    
+    post_msg: bool = False
+
+    for det_name in detectors:
+        damage_var: np.ndarray = ana.getVar(f"damage/{det_name}")
+        damage: int = len(damage_var[damage_var == 0])
+        dmg_percent: float = damage/len(damage_var)
+        if dmg_percent > post_thresh:
+            post_msg = True
+            det_entry: str = (
+                f"<tr><td><center>{det_name}</center></td>"
+                f"<td><center>{damage}</center></td>"
+                f"<td><center>{dmg_percent:.2%}</center></td></tr>"
+            )
+            table_body += det_entry
+    table_body += "</tbody>"
+    msg: str = f"<table border=\"1\">{table_header}{table_body}</table>"
+    if post_msg:
+        postElogMsg(exp=exp, msg=msg, tag=tag, title=title)
 
 ## function that chops the 64 bit time integer into something a bit more useful
 def evtt2Rt(event_time):
@@ -182,7 +340,7 @@ try:
 except:
     isStepScan = False
 
-### Fast delay stage 
+### Fast delay stage
 
 lxt_fast_his = None
 try:
@@ -199,10 +357,12 @@ ana.addCut('evr/code_137',-0.5,0.5,'hxroff')
 if ana.getFilter('hxroff').sum() >  ana.getFilter('off').sum():
     offFilter = 'hxroff'
 else:
-    offFilter = 'off'    
+    offFilter = 'off'
 nOff = ana.getFilter(offFilter).sum()
 
-#plots.
+#########################################
+# INSERT DATA QUALITY PLOTS HERE
+#########################################
 ipmUpTime = hv.HexTiles((eventTimeR[ipmUpVar<np.nanpercentile(ipmUpVar,99)],
                          ipmUpVar[ipmUpVar<np.nanpercentile(ipmUpVar,99)]),
                         kdims=[eventTimeDim, ipmUpDim]).\
@@ -211,7 +371,7 @@ ipmUpTimeMed = hv.Points((eventTimeRMed, ipmUpMed), kdims=[eventTimeDim,ipmUpDim
     options(color='r')
 ipmDownTimeMed = hv.Points((eventTimeRMed, ipmDownMed), kdims=[eventTimeDim,ipmUpDim],label=ipmDownDim.label).\
     options(color='m')
-    
+
 ipmTimeLayout = ipmUpTime*ipmUpTimeMed*ipmDownTimeMed
 
 
@@ -230,6 +390,36 @@ gspec = pn.GridSpec(sizing_mode='stretch_both', max_width=700, name='Data Qualit
 gspec[0:2,0:8] = pn.Column(ipmTimeLayout)
 gspec[2:5,0:4] = pn.Column(ipmLayout)
 
+detNames: list = ["epix_1", "Rayonix", "FEE_SPEC0", "EBeam"] # for tracking events missing data in one of these categories
+try:
+    plots = []
+    from holoviews.operation.timeseries import rolling
+    for detname in detNames:
+        damageDim = hv.Dimension((f'damage/{detname}', f'{detname} Present'))
+        damageVar = ana.getVar(damageDim.name, useFilter=iniFilter)
+        damagePlot = rolling(
+            hv.Curve(
+                damageVar,
+                vdims=[damageDim],
+                label=f'{detname}'
+            ).opts(axiswise=True, color=hv.Palette('Spectral')),
+            rolling_window=10
+        )
+        plots.append(damagePlot)
+    multiDamagePlot = hv.Overlay(plots).opts(
+        xlabel='Event (rolling average of 10)',
+        ylabel='Present (Yes/No)',
+        title='Missing/Damaged Data'
+    )
+    gspec[2:5, 4:8] = multiDamagePlot
+
+except Exception as e:
+    pass
+
+#########################################
+# Detector Images
+#########################################
+detNames = ["epix_1", "Rayonix"]
 
 detImgs=[]
 detGrids=[]
@@ -288,56 +478,174 @@ if nOff>100:
         detGrid[0,0] = pn.Row(detImgs[-1])
         detGrids.append(detGrid)
 
+########################
+# XES Droplet Plots
+#######################
+
+def processDropletData(
+        ana, #: smalldata_tools.SmallDataAna.SmallDataAna,
+        det_name: str,
+        adr_thr: Tuple[int,int] = (2, 15),
+        rot_angle: Optional[float] = None
+) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+    """Extract image and spectra from droplet data for a detector.
+    
+    Constructs an image from the 2D histogram of droplet center-of-masses.
+    The projections onto the spatial and spectral axes are then computed.
+    A number of transformations can be applied to the image prior to computing
+    the projections.
+    
+    Parameters
+    ----------
+    ana (smalldata_tools.SmallDataAna.SmallDataAna) ana object for pulling data
+        from smalldata file.
+    det_name (str) The detector to get droplet data for.
+    adu_thr (tuple) 2-tuple of lower and upper threshold to apply to the data.
+    rot_angle (float) Rotation to apply to the image prior to projections. In
+        degrees. By default, unused.
+    """
+    col = ana.getVar(f"{det}/var_full_sparse/col")
+    row = ana.getVar(f"{det}/var_full_sparse/row")
+    adu = ana.getVar(f"{det}/var_full_sparse/data")
+    n_droplets = len(adu)
+
+    # Additional thresholding.
+    adu_thr = [2, 15]
+    indices = np.where((adu >= adu_thr[0]) & (adu <= adu_thr[1]))
+
+    dimg, xedges, yedges = np.histogram2d(
+        col[indices],
+        row[indices],
+        bins=[np.arange(800),np.arange(769)],
+        weights=adu[indices]
+    )
+
+    if rot_angle:
+        from scipy.ndimage import rotate
+        dimg = rotate(dimg, angle=rot_angle)
+
+    # Calculate projections
+    spatial_proj = np.sum(dimg, axis=0)
+    xes_proj = np.sum(dimg, axis=1)
+
+    return dimg, spatial_proj, xes_proj
+
 
 xesPlots = []
-try:
-    for det in ["ePix100_1", "ePix100_2"]:
-        col = ana.getVar(f"{det}/var_full_sparse/col")
-        row = ana.getVar(f"{det}/var_full_sparse/row")
-        adu = ana.getVar(f"{det}/var_full_sparse/data")
-        n_droplets = len(adu)
-
-        adu_thr = [2, 15]
-        indices = np.where((adu >= adu_thr[0]) & (adu <= adu_thr[1]))
-
-        dimg, xedges, yedges = np.histogram2d(col[indices], row[indices], bins=[np.arange(800), np.arange(769)], weights=adu[indices])
-        dimg_flip = np.fliplr(dimg)
-
-        spatial_proj = np.sum(dimg, axis=0)
-        xes_proj = np.sum(dimg, axis=1)
+for det in ["epix_1", "epix_2"]:# detNames:
+    try:
+        dimg, spatial_proj, xes_proj = processDropletData(ana=ana, det_name=det)
+        #dimg_flip = np.fliplr(dimg)
 
         xes_grid = pn.GridSpec(max_width=700, name=f"XES - {det}")
         img_dim = hv.Dimension(("Image", "Image"))
-        xes_grid[:2, :] = hv.Image(dimg, bounds=(0, 0, dimg.shape[0], dimg.shape[1]), vdims=[img_dim]).options(colorbar=True, clim=(np.nanpercentile(dimg,1), np.nanpercentile(dimg,99.)))
         spatial_dim = hv.Dimension(("Spatial Projection", "Spatial Projection"))
-        xes_grid[2, :] = hv.Curve(spatial_proj, vdims=[spatial_dim])
         xes_dim = hv.Dimension(("XES Spectrum", "XES Spectrum"))
-        xes_grid[3, :] = hv.Curve(xes_proj, vdims=[xes_dim])
+        xes_grid[:2, :] = hv.Image(
+            dimg,
+            bounds=(0, 0, dimg.shape[0], dimg.shape[1]),
+            vdims=[img_dim]).options(colorbar=True,
+            clim=(np.nanpercentile(dimg,1),np.nanpercentile(dimg,99.))
+        ).opts(xlabel="Spatial Axis", ylabel="Spectral Axis")
+        spatial_plot = hv.Curve(spatial_proj, vdims=[spatial_dim])
+        xes_plot = hv.Curve(xes_proj, vdims=[xes_dim])
+        xes_grid[2, :] = spatial_plot.opts(
+            axiswise=True,
+            xlabel="Pixel",
+            ylabel="Sum",
+            title="Spatial Projection"
+        )
+        xes_grid[3, :] = xes_plot.opts(
+            axiswise=True,
+            xlabel="Energy (Pixel)",
+            ylabel="I",
+            title="XES Spectrum"
+        )
+
         xesPlots.append(xes_grid)
+    except Exception as e:
+        print(e)
+
+###########
+# FEE Plots
+###########
+feeGrid = pn.GridSpec(max_width=700, name="FEE Stats")
+try:
+    feeDamageDim = hv.Dimension(('damage/FEE_SPEC0','FEE Present'))
+    feeDamageVar = ana.getVar(feeDamageDim.name,useFilter=iniFilter)
+    feeDamagePlot = hv.Curve(feeDamageVar, vdims=[feeDamageDim]).opts(
+            axiswise=True,
+            xlabel="Event",
+            ylabel="FEE Present",
+            title="Damage/FEE"
+        )
+    feeGrid[:2, :2] = feeDamagePlot
 except Exception as e:
-    print(e)
+    pass
+
+try:
+    feeSpecDim = hv.Dimension(('feeBld/hproj','FEE Spec (mean)'))
+    feeSpecVar = ana.getVar(feeSpecDim.name,useFilter=iniFilter)
+    feeSpecPlot = hv.Curve(
+        np.mean(feeSpecVar, axis=0),
+        vdims=[feeSpecDim]
+    ).opts(
+            axiswise=True,
+            xlabel="Energy (Pixel)",
+            ylabel="I",
+            title="FEE Spectrum"
+        )
+    feeGrid[:2, 2:4] = feeSpecPlot
+
+    x = np.arange(feeSpecVar[0].shape[0])
+    feeCOMDim = hv.Dimension(('ProcessedFee', 'FEE COM'))
+    feeCOMVar = np.sum(feeSpecVar*x, axis=1)/np.sum(feeSpecVar, axis=1)
+    eBeamDim = hv.Dimension(('ebeam/photon_energy', 'eBeam Energy'))
+    eBeamVar = ana.getVar(eBeamDim.name,useFilter=iniFilter)
+    energyCorrPlot = hv.HexTiles(
+        (eBeamVar, feeCOMVar),
+        kdims=[eBeamDim,feeCOMDim]
+    ).opts(
+            axiswise=True,
+            xlabel="eBeam Energy",
+            ylabel="FEE COM",
+            title="FEE vs eBeam"
+        )
+    feeGrid[2:4, :2] = energyCorrPlot
+except Exception as e:
+    pass
+
+########################
+# Tabs construction and finish
+########################
 
 tabs = pn.Tabs(gspec)
-
-for detGrid in detGrids:
-    tabs.append(detGrid)
 
 for xes_grid in xesPlots:
     tabs.append(xes_grid)
 
+for detGrid in detGrids:
+    tabs.append(detGrid)
+
 if (int(os.environ.get('RUN_NUM', '-1')) > 0):
     requests.post(os.environ["JID_UPDATE_COUNTERS"], json=[{"key": "<b>BeamlineSummary Plots </b>", "value": "Done"}])
 
-#elogDir = Path(SIT_PSDM_DATA) / expname[:3] / expname / f"stats/summary/BeamlineSummary/BeamlineSummary_Run{runnum:03d}"
-elogDir = f"/sdf/data/lcls/ds/{expname[:3]}/{expname}/stats/summary/BeamlineSummary/BeamlineSummary_Run{run:03d}"
+elogDir = f"/sdf/data/lcls/ds/{expname[:3]}/{expname}/stats/summary/BeamlineSummary/BeamlineSummary_Run{run:04d}"
 
 if save_elog:
     import os
     if not os.path.isdir(elogDir):
         os.makedirs(elogDir)
     print('Made Directory to save data:', elogDir)
-    #gspec.save(('%s/report.html'%elogDir)) 
     tabs.save(('%s/report.html'%elogDir))
+        postDetectorDamageMsg(
+        detectors=detNames,
+        exp=expname,
+        run=run,
+        title=f"EVENT DAMAGE INFO- r{run:04d}",
+        smd_dir=args.directory,
+        post_thresh=0.1 # Percentage threshold to post to eLog
+    )
 
     if (int(os.environ.get('RUN_NUM', '-1')) > 0):
         requests.post(os.environ["JID_UPDATE_COUNTERS"], json=[{"key": "<b>BeamlineSummary Plots </b>", "value": "Posted"}])
