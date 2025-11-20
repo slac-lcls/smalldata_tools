e5f3a32b0db1f59ad77af6039ac3beb1ad3f5ab4
Silke Nelson <snelson@slac.stanford.edu>
2025-11-19 22:20:09 -0800

    plain producers for all lcls1 instruments

---
+++ lcls1_producers/prod_config_dia.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+# These lists are needed, do not delete them
+# If no detector in a given category, leave the corresponding
+# list empty.
+detnames = []
+
+# define functions here
+
+##########################################################
+# run independent parameters 
+##########################################################
+#aliases for experiment specific PVs go here
+# These lists are either PV names, aliases, or tuples with both.
+epicsOncePV = []
+epicsPV = []
+#fix timetool calibration if necessary
+ttCalib=[]
+
